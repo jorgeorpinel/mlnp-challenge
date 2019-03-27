523b7eec0d3f49ce494cb3dbb86af19aed27f7b2
Jorge Orpinel <jorge@orpinel.com>
2019-03-28 08:12:18 +0900

    Pseudo-code plan for app

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const http = require('http')
 const express = require('express')
+// TODO: Use SQLite as db
 
 const app = express()
 
 // app.use('/static', express.static(path.join(__dirname, 'static')))
 
+// TODO: app.post /
 app.get('/', (req, res) => {
-      console.debug('req', req)
-      console.debug('res', res)
-    }
-)
+  console.debug('req', req)
+  console.debug('res', res)
+
+  // TODO: Get POST data
+
+  // TODO: Validate URL
+
+  // TODO: Check db for pre-existing URL
+  //  Return short URL if found.
+
+  // TODO: Create new short URL
+  //  Save pair to db
+  //  Return short URL
+})
 
 server = http.createServer()
 server.on('request', app)
 
+// TODO: Start server
 console.debug('app', app)
 console.debug('server', server)
+
+// TODO: Use basic mocha tests
