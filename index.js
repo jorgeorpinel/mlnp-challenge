7cdb205daed556d91c657ed71f04220c7e92f1e4
Jorge Orpinel <jorge@orpinel.com>
2019-03-28 23:35:33 +0900

    Progress! Everything is JSON; POST / Receives and parses URLs; GET /.* is setup

--- index.js
+++ index.js
@@ -1,33 +1,87 @@
-const http = require('http')
 const express = require('express')
 // TODO: Use SQLite as db
 
 const app = express()
+app.use(express.json())
+const port = 3000
 
 // app.use('/static', express.static(path.join(__dirname, 'static')))
 
-// TODO: app.post /
-app.get('/', (req, res) => {
-  console.debug('req', req)
-  console.debug('res', res)
+/*
+ * POST /
+ * Accepts a long URL and provides a short one.
+ * Expected JSON payload input: {url:"Valid full URL."}
+ * Returns JSON object with short URL: {url: `${HOST}/short`}
+ */
+app.post('/', (req, res) => {
+  // console.debug('req.headers', req.headers)
 
-  // TODO: Get POST data
+  // 0. Get POST data
+  // console.debug('req.body', req.body)
 
-  // TODO: Validate URL
+  // console.debug('req.params', req.params)
+  // console.debug('req.query', req.query)
 
-  // TODO: Check db for pre-existing URL
+  // 1. Validate URL from payload
+  // console.debug('req.body.url', req.body.url)
+  let longURL = false
+  const url = require('url')
+  if (req.body.url)
+    longURL = url.parse(req.body.url)
+  // console.debug('Parsed URL', longURL)
+  if (!longURL || !longURL.host) {
+    res.status(400)
+    res.json({error: "No URL detected in request."})
+    return
+  }
+  // const URL = require('url').URL
+  // const myURL = new URL(req.body.url)
+  // console.debug('myURL', myURL)
+  // // ...Catch TypeError [ERR_INVALID_URL]
+
+  // TODO: 2. Check db for pre-existing URL
   //  Return short URL if found.
 
-  // TODO: Create new short URL
+  // TODO: 3. Create new short URL
   //  Save pair to db
-  //  Return short URL
+  //  Return short URL to text/plain
+  //  + or text/html depending on Accept header
+
+  // console.debug('res.url', res.url)
+  // console.debug('res._parsedUrl', res._parsedUrl)
+  // console.debug('res.statusCode', res.statusCode)
+
+  res.send('WIP POST')
 })
 
-server = http.createServer()
-server.on('request', app)
+app.get('/', function(req, res){
+  res.status(404).json({error: "Permalink required (in the request URL path)."}); // <== YOUR JSON DATA HERE
+});
 
-// TODO: Start server
-console.debug('app', app)
-console.debug('server', server)
+/*
+ * TODO: GET /{anything}
+ * Redirects to corresponding long URL.
+ */
+app.get(/\/..*/, (req, res) => {
+  // TODO: Get path from requested URL
+  console.debug(req.url)
+
+  // TODO: Sanitize and look for corresponding URL in db
+
+  // TODO: Redirect or return 404
+
+  res.send('WIP GET')
+})
+
+// Start server app
+app.listen(port, () => console.log(`Example app listening on ${port}!`))
 
 // TODO: Use basic mocha tests
+
+/**
+ * Send JSON response with any 5xx errors.
+ */
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).json({error: err.message})
+})
